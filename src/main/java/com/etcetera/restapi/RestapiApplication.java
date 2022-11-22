3472e8f88f5d90d333e89aa745f40373592496b2
afrasiabk <afrasiabkhan.work@gmail.com>
2022-11-22 18:42:45 +0500

    Initial commit

---
+++ src/main/java/com/etcetera/restapi/RestapiApplication.java
@@ -0,0 +1,24 @@
+package com.etcetera.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+	@RestController
+	public class Controller{
+
+		@GetMapping("/")
+		private String hello(){
+			return "Hello World.";
+		}
+	}
+
+}
